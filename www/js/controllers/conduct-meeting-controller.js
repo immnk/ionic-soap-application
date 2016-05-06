9c2c7c79126da18c8886b14c227fb050b5c20f8b
Mani <im.manikanta@gmail.com>
2016-05-06 12:17:22 +0530

    capture meeting screen development

--- www/js/controllers/conduct-meeting-controller.js
+++ www/js/controllers/conduct-meeting-controller.js
@@ -2,4 +2,35 @@ controllers.controller(MEETINGS.CONTROLLERS.ConductMeetingController, ConductMee
 
 ConductMeetingController.inject = ['$scope', 'utils'];
 
-function ConductMeetingController($scope, utils) {}
+function ConductMeetingController($scope, utils) {
+    $scope.record = {};
+
+    var captureError = function(e) {
+        utils.Logger.error(e);
+    }
+
+    var captureSuccess = function(e) {
+        utils.Logger.success(e);
+        console.dir(e);
+        $scope.record.file = e[0].localURL;
+        $scope.record.filePath = e[0].fullPath;
+    }
+
+    $scope.startrecording = function() {
+        navigator.device.capture.captureAudio(
+            captureSuccess, captureError, { duration: 10 });
+    }
+
+    $scope.play = function() {
+        if (!$scope.record.file) {
+            navigator.notification.alert("Record a sound first.", null, "Error");
+            return;
+        }
+        var media = new Media($scope.sound.file, function(e) {
+            media.release();
+        }, function(err) {
+            utils.Logger.error(err);
+        });
+        media.play();
+    }
+}
